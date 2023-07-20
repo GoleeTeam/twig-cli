c103cdecc0886862f1c530ffdba5cc9a757fabd7
dodo <damiandominella@gmail.com>
2023-07-20 10:22:20 -0300

    feat(): readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Twig-cli
+
+Creare un file `.html` per il template
+
+Creare un file `.json` per il data json
+
+Runnare linea di comando con questa istruzione:
+
+```bash
+node index -t <path-template> -d <path-json>
+```
+
+Esempio:
+
+```bash
+node index -t template.html -d data.json
+```
+
+in `output.html` il template compilato e aggiornato ad ogni modifica del template o del json.
--- index.js
+++ index.js
@@ -1,72 +1,75 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const {twig} = require('twig')
-const {program} = require('commander');
+const fs = require("fs");
+const { twig } = require("twig");
+const { program } = require("commander");
 
 program
-    .requiredOption('-t <path>', 'path to twig file (TWIG)')
-    .requiredOption('-d <path>', 'path to data file (JSON)')
-    .option('-o <path>', 'path to output file (HTML)')
+  .requiredOption("-t <path>", "path to twig file (TWIG)")
+  .requiredOption("-d <path>", "path to data file (JSON)")
+  .option("-o <path>", "path to output file (HTML)");
 
 program.parse();
 
 const opts = program.opts();
 
-const template = opts['t']
-console.log(`template file: ${template}`)
+const template = opts["t"];
+console.log(`template file: ${template}`);
 
-const data = opts['d']
-console.log(`data file: ${data}`)
+const data = opts["d"];
+console.log(`data file: ${data}`);
 
-const output = opts['o'] || './output.html'
-console.log(`output file: ${output}`)
+const output = opts["o"] || "./output.html";
+console.log(`output file: ${output}`);
 
-fs.watchFile(template, {persistent: true, interval: 100}, (curr, prev) => {
-    compile()
+fs.watchFile(template, { persistent: true, interval: 100 }, (curr, prev) => {
+  compile();
 });
 
-fs.watchFile(data, {persistent: true, interval: 100}, (curr, prev) => {
-    compile()
+fs.watchFile(data, { persistent: true, interval: 100 }, (curr, prev) => {
+  compile();
 });
 
-console.log(`Watching template and data files, make any change`)
+console.log(`Watching template and data files, make any change`);
 
 const compile = () => {
-    console.log(`file Changed`);
+  console.log(`file Changed`);
 
-    var templateData;
-    try {
-        templateData = JSON.parse(fs.readFileSync(data, 'utf8'));
-    } catch (e) {
-        console.error(`Error reading data file`)
-        return;
-    }
+  var templateData;
+  try {
+    templateData = JSON.parse(fs.readFileSync(data, "utf8"));
+  } catch (e) {
+    console.error(`Error reading data file`);
+    return;
+  }
 
-    var templateBuffer;
-    try {
-        const twigTemplateAsBase64 = fs.readFileSync(template, {encoding: 'base64'});
-        templateBuffer = Buffer.from(twigTemplateAsBase64, 'base64').toString('utf-8')
-    } catch (e) {
-        console.error(`Error reading template file`)
-        return;
-    }
-
-    var result;
-    try {
-        result = twig({
-            data: templateBuffer
-        }).render(templateData);
-    } catch (e) {
-        console.error(`Error compiling twig`)
-        return;
-    }
-
-    console.log(`writing output in ${output}`)
-    fs.writeFile(output, result, err => {
-        if (err) {
-            console.error(err);
-        }
+  var templateBuffer;
+  try {
+    const twigTemplateAsBase64 = fs.readFileSync(template, {
+      encoding: "base64",
     });
-}
+    templateBuffer = Buffer.from(twigTemplateAsBase64, "base64").toString(
+      "utf-8"
+    );
+  } catch (e) {
+    console.error(`Error reading template file`);
+    return;
+  }
 
+  var result;
+  try {
+    result = twig({
+      data: templateBuffer,
+    }).render(templateData);
+  } catch (e) {
+    console.error(`Error compiling twig`);
+    return;
+  }
+
+  console.log(`writing output in ${output}`);
+  fs.writeFile(output, result, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
